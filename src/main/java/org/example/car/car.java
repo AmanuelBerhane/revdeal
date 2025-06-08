07a23db1a2aebee962f0a28bfc5aee0864eff274
aman <amanuelt960@gmail.com>
2025-06-08 15:46:09 +0300

    Created car class and constructor

---
+++ src/main/java/org/example/car/car.java
@@ -0,0 +1,17 @@
+package org.example.car;
+
+public class car {
+    string name;
+    string brand;
+    int seat;
+    string VIN;
+
+    puclic static void car {
+        this.name = name;
+        this.brand = brand;
+        this.seat = seat;
+        this.VIN = VIN;
+
+    }
+}
+
